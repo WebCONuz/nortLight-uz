fdc608fc7044f6430054e27e4176cf5faa36efc0
WebCONuz <muxammadi0799@gmail.com>
2025-02-10 18:31:08 +0500

    Created modal

--- js/app.js
+++ js/app.js
@@ -9,3 +9,25 @@ openBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   menuContent.classList.remove("opened");
 });
+
+// open modal
+const authModal = document.querySelector(".modal");
+authModal.addEventListener("click", function (e) {
+  if (e.target.classList.contains("modal")) {
+    authModal.classList.remove("open");
+  }
+});
+const profileBtn = document.querySelector(".header__details.two");
+profileBtn.addEventListener("click", function () {
+  authModal.classList.add("open");
+});
+const closeModalBtn = document.querySelector(".modal__content i");
+closeModalBtn.addEventListener("click", function () {
+  authModal.classList.remove("open");
+});
+const authModalForm = document.querySelector(".modal__form");
+authModalForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("Data is sended");
+  // authModal.classList.remove("open");
+});
